e97c49f8d160e52898ae363c1dd9edfca6351f37
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-31 13:34:26 +0800

    Updated dependencies.

---
+++ .install.sh
@@ -0,0 +1,18 @@
+yarn add \
+async \
+dendron \
+diatom \
+doubt \
+falze \
+harden \
+heredito \
+kein \
+lodash \
+olivant \
+optcall \
+plough \
+protype \
+spalten \
+symbiote \
+u200b \
+--save
